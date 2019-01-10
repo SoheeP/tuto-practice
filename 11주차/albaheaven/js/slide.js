a0c82c3009f91ca2fca9aa7420952847f4b101cb
soheeP <psh950502@naver.com>
2019-01-10 19:39:36 +0900

    slide 조절해야하고 grid 끝

--- 11주차/albaheaven/js/slide.js
+++ 11주차/albaheaven/js/slide.js
@@ -1,7 +1,10 @@
-$('.slider').slick({
+$('.container').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
-  dots: true
+  dots: true,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  respondTo: 'min'
 });
