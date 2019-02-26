60299a6b0895b3cac3ac26e7ed7ce28a55e2e305
soheeP <psh950502@naver.com>
2019-02-26 19:38:29 +0900

    코드수정

--- 18주차/Study/shopping.js
+++ 18주차/Study/shopping.js
@@ -22,41 +22,42 @@ function createShopLink() {
 createShopLink();
 
 //상품 이미지 삽입
-for (let i = 0; i < (shopdb.prodList.length); i++) {//로우
+let divElBox = document.querySelector('.shop__box')
+for (let i = 0; i < (shopdb.prodList.length); i++) {//로우생성
   let divElrow = document.createElement('div');
   divElrow.classList.add('shop__row');
-  console.log(divElrow)
-  // let elProduct = document.querySelector('.shop__product')
+  let elProdRow = document.querySelectorAll('.shop__row');
+  for (let j = 0; j < 3; j++) {//shop__porduct생성
+    let ranNum = Math.floor(Math.random()*shopdb.prodList[0].list.length);
+    let divElProduct = document.createElement('div');
+    divElProduct.classList.add('shop__product');
+  
+    let aElProductLink = document.createElement('a');
+    aElProductLink.classList.add('shop__link');
+    aElProductLink.href = shopdb.prodList[0].list[ranNum].ahref;
+  
+    let imgElProduct = document.createElement('img');
+    imgElProduct.classList.add('shop__img');
+    imgElProduct.src = shopdb.prodList[0].list[ranNum].src;
+    imgElProduct.alt = shopdb.prodList[0].list[ranNum].alt;
+  
+    let pElPrTop = document.createElement('p');
+    pElPrTop.classList.add('shop__text__top');
+    pElPrTop.textContent = shopdb.prodList[0].list[ranNum].text1;
+  
+    let pElprBtm = document.createElement('p');
+    pElprBtm.classList.add('shop__text__bottom');
+    pElprBtm.textContent = shopdb.prodList[0].list[ranNum].text2;
+  
+    aElProductLink.append(imgElProduct, pElPrTop, pElprBtm);
+    divElProduct.append(aElProductLink);
+    console.log(divElProduct)
+    elProdRow[i].append(divElProduct);
+  }
+  divElBox.appendChild(elProdRow);
 }
-for (let j = 0; j < 3; j++) {
-  let ranNum = Math.floor(Math.random()*shopdb.prodList[0].list.length);
-  let divElProduct = document.createElement('div');
-  divElProduct.classList.add('shop__product');
 
-  let aElProductLink = document.createElement('a');
-  aElProductLink.classList.add('shop__link');
-  aElProductLink.href = shopdb.prodList[0].list[ranNum].ahref;
 
-  let imgElProduct = document.createElement('img');
-  imgElProduct.classList.add('shop__img');
-  imgElProduct.src = shopdb.prodList[0].list[ranNum].src;
-  imgElProduct.alt = shopdb.prodList[0].list[ranNum].alt;
-
-  let pElPrTop = document.createElement('p');
-  pElPrTop.classList.add('shop__text__top');
-  pElPrTop.textContent = shopdb.prodList[0].list[ranNum].text1;
-
-  let pElprBtm = document.createElement('p');
-  pElprBtm.classList.add('shop__text__bottom');
-  pElprBtm.textContent = shopdb.prodList[0].list[ranNum].text2;
-
-  aElProductLink.append(imgElProduct, pElPrTop, pElprBtm);
-  divElProduct.append(aElProductLink);
-  console.log(divElProduct)
-}
-let divElBox = document.getElementsByClassName('.shop__box')[0]
-let elProdRow = document.getElementsByClassName('.shop__row')
-divElBox.append(elProdRow);
 
 console.log(shopdb.prodList.length)
 
