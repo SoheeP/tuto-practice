25313ff9b02030910bc9b8ea428fc32a0ff6d8de
soheeP <psh950502@naver.com>
2019-02-27 00:13:12 +0900

    상품영역 row별 랜덤 생성 구현 - 중복값제거 필요

--- 18주차/Study/shopping.js
+++ 18주차/Study/shopping.js
@@ -22,41 +22,43 @@ function createShopLink() {
 createShopLink();
 
 //상품 이미지 삽입
-for (let i = 0; i < (shopdb.prodList.length); i++) {//로우
-  let divElrow = document.createElement('div');
-  divElrow.classList.add('shop__row');
-  console.log(divElrow)
-  // let elProduct = document.querySelector('.shop__product')
+function createShopBox() {
+  for (let i = 0; i < (shopdb.prodList.length); i++) {//로우
+    let divElrow = document.createElement('div');
+    divElrow.classList.add('shop__row');
+    let rootEl = document.querySelector('.shop__box');
+    // let elProdRow = document.getElementsByClassName('.shop__row');
+    for (let j = 0; j < 3; j++) {
+
+      let ranNum = Math.floor(Math.random() * shopdb.prodList[i].list.length);
+      let divElProduct = document.createElement('div');
+      divElProduct.classList.add('shop__product');
+
+      let aElProductLink = document.createElement('a');
+      aElProductLink.classList.add('shop__link');
+      aElProductLink.href = shopdb.prodList[i].list[ranNum].ahref;
+
+      let imgElProduct = document.createElement('img');
+      imgElProduct.classList.add('shop__img');
+      imgElProduct.src = shopdb.prodList[i].list[ranNum].src;
+      imgElProduct.alt = shopdb.prodList[i].list[ranNum].alt;
+
+      let pElPrTop = document.createElement('p');
+      pElPrTop.classList.add('shop__text__top');
+      pElPrTop.textContent = shopdb.prodList[i].list[ranNum].text1;
+
+      let pElprBtm = document.createElement('p');
+      pElprBtm.classList.add('shop__text__bottom');
+      pElprBtm.textContent = shopdb.prodList[i].list[ranNum].text2;
+
+      aElProductLink.appendChild(imgElProduct);
+      aElProductLink.appendChild(pElPrTop);
+      aElProductLink.appendChild(pElprBtm);
+      divElProduct.appendChild(aElProductLink);
+      divElrow.appendChild(divElProduct)
+    }
+    rootEl.appendChild(divElrow)
+  }
 }
-for (let j = 0; j < 3; j++) {
-  let ranNum = Math.floor(Math.random()*shopdb.prodList[0].list.length);
-  let divElProduct = document.createElement('div');
-  divElProduct.classList.add('shop__product');
 
-  let aElProductLink = document.createElement('a');
-  aElProductLink.classList.add('shop__link');
-  aElProductLink.href = shopdb.prodList[0].list[ranNum].ahref;
-
-  let imgElProduct = document.createElement('img');
-  imgElProduct.classList.add('shop__img');
-  imgElProduct.src = shopdb.prodList[0].list[ranNum].src;
-  imgElProduct.alt = shopdb.prodList[0].list[ranNum].alt;
-
-  let pElPrTop = document.createElement('p');
-  pElPrTop.classList.add('shop__text__top');
-  pElPrTop.textContent = shopdb.prodList[0].list[ranNum].text1;
-
-  let pElprBtm = document.createElement('p');
-  pElprBtm.classList.add('shop__text__bottom');
-  pElprBtm.textContent = shopdb.prodList[0].list[ranNum].text2;
-
-  aElProductLink.append(imgElProduct, pElPrTop, pElprBtm);
-  divElProduct.append(aElProductLink);
-  console.log(divElProduct)
-}
-let divElBox = document.getElementsByClassName('.shop__box')[0]
-let elProdRow = document.getElementsByClassName('.shop__row')
-divElBox.append(elProdRow);
-
-console.log(shopdb.prodList.length)
-
+createShopBox();
